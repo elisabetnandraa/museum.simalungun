de8fea7a282a6c86ed7f2d94208631a274d5ad4f
elisabetnandraa <elisabet.121140101@student.itera.ac.id>
2025-04-22 15:51:29 +0700

    pushfe

--- database/seeders/ProfilSeeder.php
+++ database/seeders/ProfilSeeder.php
@@ -15,17 +15,28 @@ class ProfilSeeder extends Seeder
     {
         $data = [
             [  
-                'deskripsi' => ' Museum Simalungun dibangun pada tanggal 10 April 1939 di Pematangsiantar oleh Raja-raja Simalungun dengan menggunakan 
-                biaya sebesar 1.650 Gulden dan diresmikan pada tanggal 30 April 1940. Museum Simalungun dibangun dengan bentuk yang menyerupai 
-                rumah adat simalungun yang berbentuk rumah panggung. Ada 
-                keunikan yang ada pada bangunan Rumah Bolon atau Museum 
-                Simalungun. Museum Simalungun itu berdiri dengan kokoh dengan 
-                beberapa tiang yang terbuat dari kayu-kayu keras yang mempunyai 
-                kualitas tinggi. Karena bangunan ini berbentuk rumah 
-                panggung,tentunya untuk masuk ke dalam museum tersebut 
-                adalah dengan tangga. Tangga nya juga terbuat dari papan kayu 
-                yang keras sehingga tidak mudah rapuh. Dan ada dibuat tali rotan 
-                sebagai pegangan untuk menaiki tangga tersebut.',
+                'deskripsi' => ' Museum Simalungun dibangun pada tanggal 10 April 1939 di Pematangsiantar oleh para Raja Simalungun 
+                dengan biaya sebesar 1.650 Gulden, dan diresmikan pada tanggal 30 April 1940.Bangunan museum ini dirancang menyerupai 
+                rumah adat Simalungun, yaitu rumah panggung yang memiliki keunikan tersendiri. Museum Simalungun berdiri kokoh dengan 
+                beberapa tiang yang terbuat dari kayu keras berkualitas tinggi.Karena berbentuk rumah panggung, akses masuk ke dalam 
+                museum menggunakan tangga yang juga terbuat dari papan kayu keras sehingga tidak mudah rapuh. Sebagai pegangan saat
+                menaiki tangga, disediakan tali rotan yang diikat kuat.Gedung museum ini menjadi bagian penting dalam sejarah permuseuman
+                karena sejak diresmikan pada 30 April 1940 hingga dibongkar pada 19 Oktober 1981, telah mencatat lebih dari 40 tahun keberadaan 
+                yang bermutu dalam dunia permuseuman. Koleksi barang-barang purbakala yang dimiliki Museum Simalungun menjadi daya tarik bagi 
+                pengunjung, baik dari dalam maupun luar negeri. Berdasarkan catatan buku tamu dari tahun 1972 hingga Juni 1982, 
+                jumlah pengunjung mencapai lebih dari 40.000 orang yang sebagian besar mengunjungi Rumah Bolon Adat Purba. Sumbangan dari para
+                pengunjung tersebut mencapai Rp1.788.385,00. Perhatian dari instansi pemerintah, khususnya Departemen Pendidikan dan Kebudayaan 
+                (Permuseuman), turut menjadi dorongan penting bagi pengurus museum, terutama dalam bentuk bantuan dan arahan. Salah satunya melalui 
+                surat dari Menteri Muda Permuseuman, Drs. Kosmas Batubara, tertanggal 5 Juni 1982 No. 03/04, yang berisi himbauan agar seluruh potensi 
+                yang ada di daerah dapat dimanfaatkan secara optimal. Demikian pula, hasil studi kelayakan Kanwil Departemen P dan K Provinsi Sumatra 
+                Utara di Pematangsiantar pada tanggal 7 Juni 1982, yang disusun oleh Kepala Bidang Permuseuman Provinsi Sumatra Utara Drs. E.K. Siahaan 
+                beserta staf Drs. J. Saragih, dinilai cukup baik dan dapat dijadikan pedoman oleh Pengurus Yayasan Museum Simalungun. Isi studi tersebut 
+                mencakup rencana pembinaan agar Museum Simalungun dapat berkembang sesuai dengan ketentuan permuseuman yang berlaku saat ini. Dalam waktu 
+                yang tidak lama, museum ini juga direncanakan akan menerima bantuan tambahan sesuai dengan permohonan Pengurus Yayasan Museum Simalungun 
+                melalui surat tertanggal 28 Juli 1982 (S. Purba), Bidang Proyek. Selain itu, berdasarkan saran dari Bupati KDH Tingkat II Simalungun, 
+                J.P. Silitonga, enam kios milik Yayasan Museum Simalungun akan dipugar dan dibangun ulang dengan desain khas daerah Simalungun, seperti 
+                patung dan ukiran. Upaya ini bertujuan menjadikan Museum Simalungun sebagai simbol budaya dan daya tarik bagi masyarakat lokal, serta 
+                lebih menarik minat wisatawan dari dalam maupun luar negeri, sehingga citra museum akan jauh lebih baik dibandingkan tahun-tahun sebelumnya.',
                 'gambar' => 'assets/img/profil1.png',
                 'created_at' => now(),
                 'updated_at' => now(),
--- database/seeders/SubProfilSeeder.php
+++ database/seeders/SubProfilSeeder.php
@@ -16,20 +16,20 @@ class SubProfilSeeder extends Seeder
     {
         $data = [
             [
-                'penjelasan' => ' Museum Simalungun adalah museum tertua di Sumatera Utara
-                dan Museum ini terletak di Kota Pematang Siantar. Museum
-                Simalungun dibangun pada tanggal 10 April 1939 di
-                Pematangsiantar oleh Raja-raja Simalungun dengan
-                menggunakan biaya sebesar 1.650 Gulden dan diresmikan pada
-                tanggal 30 April 1940. Museum ini dibangun Raja Marpitu
-                Simalungun,yakni ( Raja Siantar,sauhaluh damanik,Raja
-                Tanah Jawa Sinaga,Raja Pakek Purba dasuha,Raja Dolok Silou
-                Purba Tambak,Raja Raya Pondaihaim saragih garingging,Raja
-                Purba purba pak-pak,Dan raja silimakuta Purba girsang ).  Museum Simalungun dibangun dengan bentuk yang
-                menyerupai rumah adat simalungun yang berbentuk rumah
-                panggung. Jumlah koleksi Museum Simalungun berjumlah 860
-                buah, dimana seluruhnya tertata rapi dalam vitrine atau lemari
-                pajangan Museum Simalungun.',
+                'penjelasan' => ' Museum Simalungun didirikan atas prakarsa tujuh Raja Simalungun bersama perwakilan pemerintah, 
+                tokoh masyarakat, kepala distrik, serta tungkat yang berkumpul dalam pertemuan adat Harungguan. Pada awal berdirinya, 
+                museum ini dikenal dengan nama Rumah Pusaka Simalungun. Namun, seiring waktu dan perkembangan zaman, namanya berubah 
+                menjadi Museum Simalungun. Pembangunan museum selesai pada bulan Desember 1939 dan secara resmi dibuka untuk umum pada 
+                tanggal 30 April 1940. Pengelolaan museum secara profesional dimulai 14 tahun kemudian oleh Yayasan Museum Simalungun, 
+                yang berdiri sejak 27 September 1954.Dalam rangka peresmian museum, para Raja Marpitu turut menyumbangkan berbagai benda 
+                bersejarah sebagai koleksi, seperti Pustaha Lak-lak, patung-patung batu dari masa megalitikum, perlengkapan dapur, 
+                alat makan, peralatan tenun, perhiasan dari emas dan perak, koin, serta uang kuno. Oleh karena itu, Museum Simalungun 
+                tidak hanya menjadi tempat penyimpanan benda bersejarah, tetapi juga menjadi saksi penting perjalanan sejarah masyarakat 
+                Simalungun. Kepedulian para raja saat itu menjadi bukti nyata terhadap upaya pewarisan budaya kepada generasi masa depan.
+                Pendirian museum ini dilandasi semangat untuk menjaga serta melestarikan benda-benda bersejarah agar tidak punah oleh arus 
+                perubahan zaman. Sejak 7 Juni 1955, pengelolaan museum secara resmi berada di bawah naungan Yayasan Museum Simalungun. 
+                Kegiatan pemeliharaan dan perawatan museum dibiayai dari donasi pengunjung serta dukungan pemerintah Kabupaten Simalungun 
+                dan Pemerintah Kota Pematangsiantar.',
                 'gambar' => 'assets/img/profil2.png',
                 'created_at' => now(),
                 'updated_at' => now(),
